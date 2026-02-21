53aba570589db840c12d55d0f82043dfcd2a6c6e
juanalexismartinez557-a11y <juanalexismartinez557@gmail.com>
2026-02-20 20:18:39 -0600

    (EJERCICIO 5): se añadio la libreria chrono para medir el tiempo de cada funcion

--- busquedaOrdenamiento/busquedaOrdenamiento/busquedaOrdenamiento.cpp
+++ busquedaOrdenamiento/busquedaOrdenamiento/busquedaOrdenamiento.cpp
@@ -1,5 +1,7 @@
 ﻿#include <iostream>
+#include <chrono>
 using namespace std;
+using namespace std::chrono;
 
 #include "search.h"
 #include "sort.h"
@@ -22,8 +24,7 @@ int main()
 
     bool mostrarProceso;
 
-	// aqui se muestra el menu de generacion de numeros y se generan los datos
-
+    // Configuración inicial
     cout << "=== CONFIGURACION INICIAL ===\n";
 
     int opGen = menuGeneracion();   // repetidos o no
@@ -42,7 +43,6 @@ int main()
     cout << "\nNumeros generados:\n";
     mostrarArreglo(arreglo, n);
 
-	// aqui pregunta si se desea mostrar el proceso de los algoritmos o no
     cout << "\n¿Deseas ver el proceso de los algoritmos? (1=Si, 0=No): ";
     cin >> mostrarProceso;
 
@@ -55,36 +55,71 @@ int main()
         switch (opcion)
         {
         case 1: // Busqueda Secuencial
+        {
             mostrarArreglo(arreglo, n);
             valor = pedirValor();
+
+            cout << "Cronometro iniciado (Busqueda Secuencial)...\n";
+            auto inicio = high_resolution_clock::now();
+
             posicion = busquedaSecuencial(arreglo, n, valor, mostrarProceso);
 
+            auto fin = high_resolution_clock::now();
+            duration<double, milli> tiempo = fin - inicio;
+
             if (posicion != -1)
                 cout << "Valor encontrado en posicion: " << posicion << endl;
             else
                 cout << "Valor no encontrado.\n";
+
+            cout << "Tiempo de ejecucion: " << tiempo.count() << " ms\n";
             break;
+        }
 
         case 2: // Busqueda Binaria
+        {
             cout << "\nOrdenando arreglo con Burbuja...\n";
+
+            cout << "Cronometro iniciado (Ordenamiento Burbuja)...\n";
+            auto inicioOrden = high_resolution_clock::now();
+
             ordenarBurbuja(arreglo, n, mostrarProceso);
+
+            auto finOrden = high_resolution_clock::now();
+            duration<double, milli> tiempoOrden = finOrden - inicioOrden;
+
+            cout << "Tiempo Burbuja: " << tiempoOrden.count() << " ms\n";
 
             mostrarArreglo(arreglo, n);
 
             valor = pedirValor();
+
+            cout << "Cronometro iniciado (Busqueda Binaria)...\n";
+            auto inicioBusq = high_resolution_clock::now();
+
             posicion = busquedaBinaria(arreglo, n, valor, mostrarProceso);
+
+            auto finBusq = high_resolution_clock::now();
+            duration<double, milli> tiempoBusq = finBusq - inicioBusq;
 
             if (posicion != -1)
                 cout << "Valor encontrado en posicion: " << posicion << endl;
             else
                 cout << "Valor no encontrado.\n";
+
+            cout << "Tiempo Busqueda Binaria: " << tiempoBusq.count() << " ms\n";
             break;
+        }
 
         case 3: // Ordenar arreglo
+        {
             metodo = mostrarMenuOrdenamiento();
 
             comparaciones = 0;
             intercambios = 0;
+
+            cout << "Cronometro iniciado (Ordenamiento)...\n";
+            auto inicio = high_resolution_clock::now();
 
             if (metodo == 1)
             {
@@ -114,9 +149,15 @@ int main()
                 cout << "Metodo invalido.\n";
             }
 
+            auto fin = high_resolution_clock::now();
+            duration<double, milli> tiempo = fin - inicio;
+
+            cout << "\nTiempo de ejecucion: " << tiempo.count() << " ms\n";
+
             cout << "\nArreglo ordenado:\n";
             mostrarArreglo(arreglo, n);
             break;
+        }
 
         case 4:
             cout << "Saliendo...\n";

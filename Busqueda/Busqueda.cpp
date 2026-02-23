d18834e396bc67fcb37f287020fd028e6d0e4d50
lchavez50059-pixel <lchavez50059@ucq.edu.mx>
2026-02-23 17:01:20 -0600

    .
    .

--- Busqueda/Busqueda.cpp
+++ Busqueda/Busqueda.cpp
@@ -1,38 +1,24 @@
-#include <iostream>
-using namespace std;
+#include "Busqueda.h"
 
-// Función de búsqueda binaria
-int busquedaBinaria(int arr[], int n, int objetivo) {
-    int inicio = 0, fin = n - 1;
+int Busqueda::busquedaBinaria(int arr[], int n, int objetivo) {
+
+    int inicio = 0;
+    int fin = n - 1;
+
     while (inicio <= fin) {
+
         int medio = inicio + (fin - inicio) / 2;
+
         if (arr[medio] == objetivo) {
-            return medio; // Elemento encontrado
+            return medio;
         }
         else if (arr[medio] < objetivo) {
-            inicio = medio + 1; // Buscar en la mitad derecha
+            inicio = medio + 1;
         }
         else {
-            fin = medio - 1; // Buscar en la mitad izquierda
+            fin = medio - 1;
         }
     }
-    return -1; // Elemento no encontrado
-}
 
-int main() {
-    // Arreglo ordenado
-    int arr[] = { 10, 12, 15, 25, 50, 100 };
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int objetivo = 17;
-
-    int resultado = busquedaBinaria(arr, n, objetivo);
-
-    if (resultado != -1) {
-        cout << "Elemento encontrado en la posición: " << resultado << endl;
-    }
-    else {
-        cout << "Elemento no encontrado." << endl;
-    }
-
-    return 0;
+    return -1;
 }

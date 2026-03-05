e1d9c6876a46a8f6c3a8d6948f5c9a33d704f942
juanalexismartinez557-a11y <juanalexismartinez557@gmail.com>
2026-03-04 22:04:31 -0600

    Programación Modular

--- busquedaOrdenamiento/busquedaOrdenamiento/Busqueda.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-class Busqueda {
-public:
-    int busquedaBinaria(int arr[], int n, int objetivo);
-};
--- busquedaOrdenamiento/busquedaOrdenamiento/search.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#ifndef SEARCH_H
-#define SEARCH_H
-
-int busquedaSecuencial(int arreglo[], int n, int valor, bool mostrarProceso);
-int busquedaBinaria(int arreglo[], int n, int valor, bool mostrarProceso);
-
-#endif
